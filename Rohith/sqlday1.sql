cab34b0ee6d6f8e1a72637c6dd6cd91e00d0e840
rohith vuppula <rohithvuppula@gmail.com>
2026-06-08 15:57:06 +0530

    added new queries

--- Rohith/sqlday1.sql
+++ Rohith/sqlday1.sql
@@ -1,4 +1,21 @@
 -- Day 1: SELECT, WHERE, ORDER BY
 -- Starting SQL sprint - June 8, 2026
+-- Day 1: SELECT and WHERE
 
-SELECT 'SQL sprint starts tomorrow' AS message;
+-- 1. Get all students
+SELECT * FROM students;
+
+-- 2. Get only name and marks
+SELECT name, marks FROM students;
+
+-- 3. Students who scored above 80
+SELECT name, marks FROM students 
+WHERE marks > 80;
+
+-- 4. Students from Hyderabad
+SELECT * FROM students 
+WHERE city = 'Hyderabad';
+
+-- 5. Students above 80 from Hyderabad
+SELECT name, marks FROM students 
+WHERE marks > 80 AND city = 'Hyderabad';
